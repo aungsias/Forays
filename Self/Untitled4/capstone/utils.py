a4f25b3f91bd8cb1843e87e15a172ea68c6554ca
aungsias <106785278+aungsias@users.noreply.github.com>
2023-09-13 16:44:07 -0700

    Updating Repo

--- Self/Untitled4/capstone/utils.py
+++ Self/Untitled4/capstone/utils.py
@@ -1,4 +1,12 @@
 import pandas as pd
+import pickle
 
-def read_file(path, name, index_col=None):
-    return pd.read_csv(f"{path}/{name}.csv", index_col=index_col, parse_dates=True)
+def read_file(file_name, path=None, index_col=None):
+    if not path:
+        return pd.read_csv(f"data/{file_name}.csv", index_col=index_col, parse_dates=True)
+    else:
+        return pd.read_csv(f"{path}/{file_name}.csv", index_col=index_col, parse_dates=True)
+    
+def get_sectors():
+    with open("data/sector_list.pkl", "rb") as f:
+        return pickle.load(f)
